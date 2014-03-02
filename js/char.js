268b125c97edc827e016f10f1851875e1ed47e4a
Rafa D. Latorre López Villalta <sauco82@gmail.com>
2014-03-02 10:50:50 +0100

    hero moving

--- js/char.js
+++ js/char.js
@@ -2,29 +2,36 @@
 	function Hero(image) {
 		this.initialize(image);
 	}
+ 	Hero.prototype = new createjs.Sprite();
+ 	Hero.prototype.Sprite_initialize = Hero.prototype.initialize;
+
 
  	Hero.prototype.initialize = function (image) {
- 		var data = {
- 			framerate: 20,
+ 		var spriteSheet = new createjs.SpriteSheet({
+ 			framerate: 2,
  			images: [image],
  			frames: {
- 				width: 65,
+ 				width: 64,
  				height: 94,
  				count:12
  			},
  			animations: {
  				down: [0,2],
- 				up: [3,5],
- 				left: [6,9],
- 				right: [10,12]
+ 				up: {frames:[3,5]},
+ 				left: [6,8],
+ 				right: [9,11],
+ 				down_idle: {frames:[1]},
+ 				up_idle: {frames:[4]},
+ 				left_idle: {frames:[7]},
+ 				rigth_idle: {frames:[10]}
  			}
- 		}
-
- 		this.spriteSheet = new createjs.SpriteSheet(data);
+ 		});
+ 		
+ 		this.Sprite_initialize(spriteSheet);
  		this.name = 'Hero';
  		this.snapToPixel = true;
 
- 		this.up = new createjs.Sprite(this.spriteSheet, "up");
+ 		this.gotoAndPlay("left_idle");   
  	}	
 
  	window.Hero = Hero;
